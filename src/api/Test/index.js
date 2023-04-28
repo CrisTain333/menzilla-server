78e255f26ba384b0e94b0d575b6a1ecc06d68587
Sukanta Das <arnobh84@gmail.com>
2023-04-28 09:52:17 +0600

    some more change

--- src/api/Test/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { testController } = require("../../controller/testController");
-const router = express.Router();
-
-router.use("/", testController);
-
-module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const testRoute = require("../api/Test/index");
 const authRoute = require("../api/auth/index");
 const userRoute = require("../api/user/index");
 
-router.use("/test", testRoute);
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 
