a5e7f58c6cea1c43fc99e325c6ddd294fbcd8b9e
Sukanta Das <arnobh84@gmail.com>
2023-04-28 10:24:51 +0600

    get user added

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,9 +1,12 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/UserModel");
+
 exports.handleGetUser = async (req, res) => {
-  return {
-    status: 200,
-    message: "ok",
-    user: {
-      name: "sukan",
-    },
-  };
+  const auth_Token = req.header("Authorization");
+  const token = auth_Token.split(" ")[1];
+
+  const id = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await UserModel.findById(id.userId);
+
+  return { status: 200, message: "success", user };
 };
