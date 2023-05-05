07004ef4e29a5ee5836bb90b911f88a12424e704
Sukanta Das <arnobh84@gmail.com>
2023-05-05 20:28:45 +0600

    all fileds cheking added

--- src/services/shopServices.js
+++ src/services/shopServices.js
@@ -7,6 +7,30 @@ exports.handleShopRegister = async (req, res) => {
     const shopData = req.body;
     const file = req.file;
 
+    const { shopName, email, password, address, phone, shopProfile, zipCode } =
+      shopData;
+
+    if (!shopName) {
+      return { status: 400, message: "shope name  is  required" };
+    } else if (!email) {
+      return { status: 400, message: "email is  required" };
+    } else if (!password) {
+      return { status: 400, message: "password is  required" };
+    } else if (!address) {
+      return { status: 400, message: "address is  required" };
+    } else if (!phone) {
+      return { status: 400, message: "phone is  required" };
+    } else if (!shopProfile) {
+      return { status: 400, message: "shop profile is  required" };
+    } else if (!zipCode) {
+      return { status: 400, message: "zipCode  is  required" };
+    }
+
+    const existingUser = await ShopModal.findOne({ $or: [{ email }] });
+    if (existingUser) {
+      return { status: 400, message: "Email already in use" };
+    }
+
     const bucket = config.storage().bucket(process.env.STORAGE_BUCKET);
     const folderName = "Menzilla-storage/";
     const fileUpload = bucket.file(folderName + file.originalname);
@@ -43,6 +67,7 @@ exports.handleShopRegister = async (req, res) => {
 
     return { status: 201, message: "shop created successfully" };
   } catch (error) {
+    console.log(error);
     return {
       status: 500,
       message: error.toString(),
