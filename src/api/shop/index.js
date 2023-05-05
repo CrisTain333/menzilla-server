b9319739bf181dfe195915a2d961213159326ab2
Sukanta Das <arnobh84@gmail.com>
2023-05-05 18:42:22 +0600

    storage bucket setup completed

--- src/api/shop/index.js
+++ src/api/shop/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { registerShop } = require("../../controller/shopController");
+const upload = require("../../middleware/upload");
 
-router.post("/register", registerShop);
+router.post("/register", upload.single("shopProfile"), registerShop);
 
 module.exports = router;
