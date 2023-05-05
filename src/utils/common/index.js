b47f31dc502b33ae28d78b8e5dc99a09e80bd277
Sukanta Das <arnobh84@gmail.com>
2023-05-05 18:24:05 +0600

    shop  modal created

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -30,133 +30,411 @@ const sendEmail = async (options, activationUrl) => {
       sender,
       to: receivers,
       htmlContent: `
-			<head>
-  <title></title>
-  <!--[if !mso]><!-- -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!--<![endif]-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<style type="text/css">
-  #outlook a { padding: 0; }
-  .ReadMsgBody { width: 100%; }
-  .ExternalClass { width: 100%; }
-  .ExternalClass * { line-height:100%; }
-  body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
-  table, td { border-collapse:collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
-  img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
-  p { display: block; margin: 13px 0; }
-</style>
-<!--[if !mso]><!-->
-<style type="text/css">
-  @media only screen and (max-width:480px) {
-    @-ms-viewport { width:320px; }
-    @viewport { width:320px; }
-  }
-</style>
-<!--<![endif]-->
-<!--[if mso]>
-<xml>
-  <o:OfficeDocumentSettings>
-    <o:AllowPNG/>
-    <o:PixelsPerInch>96</o:PixelsPerInch>
-  </o:OfficeDocumentSettings>
-</xml>
-<![endif]-->
-<!--[if lte mso 11]>
-<style type="text/css">
-  .outlook-group-fix {
-    width:100% !important;
-  }
-</style>
-<![endif]-->
-
-<!--[if !mso]><!-->
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+      
+      <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
+        body {
+            margin: 0;
+            background: #FEFEFE;
+            color: #585858;
+        }
 
-        @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+        table {
+            font-size: 15px;
+            line-height: 23px;
+            max-width: 500px;
+            min-width: 460px;
+            text-align: center;
+        }
 
+        .table_inner {
+            min-width: 100% !important;
+        }
+
+        td {
+            font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+            vertical-align: top;
+        }
+
+        .carpool_logo {
+            margin: 30px auto;
+        }
+
+        .dummy_row {
+            padding-top: 20px !important;
+        }
+
+        .section,
+        .sectionlike {
+            background: #C9F9E9;
+        }
+
+        .section {
+            padding: 0 20px;
+        }
+
+        .sectionlike {
+            padding-bottom: 10px;
+        }
+
+        .section_content {
+            width: 100%;
+            background: #fff;
+        }
+
+        .section_content_padded {
+            padding: 0 35px 40px;
+        }
+
+        .section_zag {
+            background: #F4FBF9;
+        }
+
+        .imageless_section {
+            padding-bottom: 20px;
+        }
+
+        img {
+            display: flex;
+            margin: 0 auto;
+        }
+
+        .img_section {
+            width: 100%;
+            max-width: 500px;
+        }
+
+        .img_section_side_table {
+            width: 100% !important;
+        }
+
+        h1 {
+            font-size: 20px;
+            font-weight: 500;
+            margin-top: 40px;
+            margin-bottom: 0;
+        }
+
+        .near_title {
+            margin-top: 10px;
+        }
+
+        .last {
+            margin-bottom: 0;
+        }
+
+        a {
+            color: #ff9900;
+            font-weight: 500;
+            word-break: break-word;
+            /* Footer has long unsubscribe link */
+        }
+
+        .button {
+            display: block;
+            width: 100%;
+            max-width: 300px;
+            background: #ff9900;
+            border-radius: 8px;
+            color: #fff;
+            font-size: 18px;
+            font-weight: normal;
+            /* Resetting from a */
+            padding: 12px 0;
+            margin: 30px auto 0;
+            text-decoration: none;
+        }
+
+        small {
+            display: block;
+            width: 100%;
+            max-width: 330px;
+            margin: 14px auto 0;
+            font-size: 14px;
+        }
+
+        .signature {
+            padding: 20px;
+        }
+
+        .footer,
+        .footer_like {
+            background: #ff9900;
+        }
+
+        .footer {
+            padding: 0 20px 30px;
+        }
+
+        .footer_content {
+            width: 100%;
+            text-align: center;
+            font-size: 12px;
+            line-height: initial;
+            color: #005750;
+        }
+
+        .footer_content a {
+            color: #005750;
+        }
+
+        .footer_item_image {
+            margin: 0 auto 10px;
+        }
+
+        .footer_item_caption {
+            margin: 0 auto;
+        }
+
+        .footer_legal {
+            padding: 20px 0 40px;
+            margin: 0;
+            font-size: 12px;
+            color: #A5A5A5;
+            line-height: 1.5;
+        }
+
+        .text_left {
+            text-align: left;
+        }
+
+        .text_right {
+            text-align: right;
+        }
+
+        .va {
+            vertical-align: middle;
+        }
+
+        .stats {
+            min-width: auto !important;
+            max-width: 370px;
+            margin: 30px auto 0;
+        }
+
+        .counter {
+            font-size: 22px;
+        }
+
+        .stats_counter {
+            width: 23%;
+        }
+
+        .stats_image {
+            width: 18%;
+            padding: 0 10px;
+        }
+
+        .stats_meta {
+            width: 59%;
+        }
+
+        .stats_spaced {
+            padding-top: 16px;
+        }
+
+        .walkthrough_spaced {
+            padding-top: 24px;
+        }
+
+        .walkthrough {
+            max-width: none;
+        }
+
+        .walkthrough_meta {
+            padding-left: 20px;
+        }
+
+        .table_checkmark {
+            padding-top: 30px;
+        }
+
+        .table_checkmark_item {
+            font-size: 15px;
+        }
+
+        .td_checkmark {
+            width: 24px;
+            padding: 7px 12px 0 0;
+        }
+
+        .padded_bottom {
+            padding-bottom: 40px;
+        }
+
+        .marginless {
+            margin: 0;
+        }
+
+        /* Restricting responsive for iOS Mail app only as Inbox/Gmail have render bugs */
+        @media only screen and (max-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+            table {
+                min-width: auto !important;
+            }
+
+            .section_content_padded {
+                padding-right: 25px !important;
+                padding-left: 25px !important;
+            }
+
+            .counter {
+                font-size: 18px !important;
+            }
+        }
     </style>
-  <!--<![endif]--><style type="text/css">
-  @media only screen and (min-width:480px) {
-    .mj-column-per-100, * [aria-labelledby="mj-column-per-100"] { width:100%!important; }
-  }
-</style>
 </head>
-<body style="background: #F9F9F9;">
-  <div style="background-color:#F9F9F9;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]-->
-  <style type="text/css">
-    html, body, * {
-      -webkit-text-size-adjust: none;
-      text-size-adjust: none;
-    }
-    a {
-      color:#1EB0F4;
-      text-decoration:none;
-    }
-    a:hover {
-      text-decoration:underline;
-    }
-  </style>
 
-<div style="max-width:640px;margin:0 auto;box-shadow:0px 1px 5px rgba(0,0,0,0.1);border-radius:4px;overflow:hidden"><div style="margin:0px auto;max-width:640px;background:#7289DA url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png) top center / cover no-repeat;"><!--[if mso | IE]>
-      <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:640px;">
-        <v:fill origin="0.5, 0" position="0.5,0" type="tile" src="https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png" />
-        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
-      <![endif]--><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#7289DA url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png) top center / cover no-repeat;" align="center" border="0" background="https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:57px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:undefined;width:640px;">
-      <![endif]--><div style="cursor:auto;color:white;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:36px;font-weight:600;line-height:36px;text-align:center;">Welcome to Menzilla!</div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table><!--[if mso | IE]>
-        </v:textbox>
-      </v:rect>
-      <![endif]--></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="margin:0px auto;max-width:640px;background:#ffffff;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#ffffff;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 70px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px 0px 20px;" align="left"><div style="cursor:auto;color:#737F8D;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:16px;line-height:24px;text-align:left;">
-            <p><img src="https://cdn.discordapp.com/email_assets/127c95bbea39cd4bc1ad87d1500ae27d.png" alt="Party Wumpus" title="None" width="500" style="height: auto;"></p>
+<body style="	margin: 0;
+	background: #FEFEFE;
+	color: #585858;
+">
+    <!-- Preivew text -->
+    <span class="preheader"
+        style="display: none !important; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;border-collapse: collapse;border: 0px;"></span>
+    <!-- Carpool logo -->
+    <table align="center" border="0" cellspacing="0" cellpadding="0" style="	font-size: 15px;
+	line-height: 23px;
+	max-width: 500px;
+	min-width: 460px;
+	text-align: center;
+">
+        <tbody>
+            <tr>
+                <td style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+  
+    border: none !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+">
+                    <img src="https://i.ibb.co/yd69Dkw/letter-m-logo-design-with-black-orange-color-and-circle-cool-modern-icon-letters-logo-vector-removeb.png"
+                        class="carpool_logo" style="    display: flex;
+    margin: 0 auto;
+    margin: 30px 4px;
+    height: 70px;
+    width: 70px;">
+                    <span
+                        style="font-size:xx-large; font-weight: 900; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color: black;">enZilla</span>
+                </td>
+            </tr>
+            <!-- Header -->
+            <tr>
+                <td class="sectionlike imageless_section" style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+  padding-bottom: 10px;
+padding-bottom: 20px;"></td>
+            </tr>
+            <!-- Content -->
+            <tr>
+                <td class="section" style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+	padding: 0 20px;
+">
+                    <table border="0" cellspacing="0" cellpadding="0" class="section_content" style="	font-size: 15px;
+	line-height: 23px;
+	max-width: 500px;
+	min-width: 460px;
+	text-align: center;
+	width: 100%;
+	background: #fff;
+">
+                        <tbody>
+                            <tr>
+                                <td class="section_content_padded" style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+padding: 0 35px 40px;">
+                                    <h1 style="	font-size: 20px;
+	font-weight: 500;
+	margin-top: 40px;
+	margin-bottom: 0;
+">Hi ${options?.name}</h1>
+                                    <p class="near_title last" style="margin-top: 10px;margin-bottom: 0;">Please verify
+                                        that your email address is ${options?.email}, and that you entered it when
+                                        signing up for Menzilla.</p>
+                                    <a href=${activationUrl} style="	display: block;
+	width: 100%;
+	max-width: 300px;
+	background: #ff9900;
+	border-radius: 8px;
+	color: #fff;
+	font-size: 18px;
+	padding: 12px 0;
+	margin: 30px auto 0;
+	text-decoration: none;
+" target="_blank">Verify email</a>
+                                    <small style="	display: block;
+	width: 100%;
+	max-width: 330px;
+	margin: 14px auto 0;
+	font-size: 14px;
+">This link will expire in 24 hours</small>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <!-- Signature -->
+            <tr>
+                <td class="section" style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+	padding: 0 20px;
+">
+                    <table border="0" cellspacing="0" cellpadding="0" class="section_content section_zag" style="	font-size: 15px;
+	line-height: 23px;
+	max-width: 500px;
+	min-width: 460px;
+	text-align: center;
+	width: 100%;
+	background: #fff;
+background: #F4FBF9;">
+                        <tbody>
+                            <tr>
+                                <td class="signature" style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+padding: 20px;">
+                                    <p class="marginless" style="margin: 0;">Happy Shopping, <br>The Menzilla Team
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <!-- Legal footer -->
+            <tr>
+                <td style="	font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+	vertical-align: top;
+    border: none !important;
+">
+                    <p class="footer_legal" style="	padding: 20px 0 40px;
+	margin: 0;
+	font-size: 12px;
+	color: #A5A5A5;
+	line-height: 1.5;
+">
+                        If you did not enter this email address when signing up for Menzilla, disregard this
+                        message.<br><br>
+                        © 2023 Google Inc. 1600 Amphitheatre Parkway, Mountain View, CA 94043
+                        <br><br>
 
-  <h2 style="font-family: Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-weight: 500;font-size: 20px;color: #4F545C;letter-spacing: 0.27px;">Hey ${options?.name}</h2>
-<p>We're happy you're here. Let's get your email address verified</p>
-<p>This Link Experis in 24 hours</p>
-
-          </div></td></tr><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center"><table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0"><tbody><tr><td style="border:none;border-radius:3px;color:white;cursor:auto;padding:15px 19px;" align="center" valign="middle" bgcolor="#7289DA"><a href=${activationUrl} target="_blank" style="text-decoration:none;line-height:100%;background:#7289DA;color:white;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:15px;font-weight:normal;text-transform:none;margin:0px;" target="_blank">
-            Verify Email
-          </a></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--></div><div style="margin:0px auto;max-width:640px;background:transparent;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:0px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;"><div style="font-size:1px;line-height:12px;">&nbsp;</div></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="margin:0 auto;max-width:640px;background:#ffffff;box-shadow:0px 1px 5px rgba(0,0,0,0.1);border-radius:4px;overflow:hidden;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#ffffff;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:0px;"><!--
-    
+                        This is a mandatory service email from Menzilla.
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 </body>
-		`,
+      `,
     })
     .then((data) => {
       return data;
