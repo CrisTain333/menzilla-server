c1463966918f2e1ae07a2c6ac4159ad107be0439
Sukanta Das <arnobh84@gmail.com>
2023-05-05 19:40:38 +0600

    shop services codebase changed

--- src/services/shopServices.js
+++ src/services/shopServices.js
@@ -3,42 +3,49 @@ const ShopModal = require("../models/ShopModal");
 const bcrypt = require("bcrypt");
 
 exports.handleShopRegister = async (req, res) => {
-  const shopData = req.body;
-  const file = req.file;
+  try {
+    const shopData = req.body;
+    const file = req.file;
 
-  const bucket = config.storage().bucket(process.env.STORAGE_BUCKET);
-  const folderName = "Menzilla-storage/";
-  const fileUpload = bucket.file(folderName + file.originalname);
+    const bucket = config.storage().bucket(process.env.STORAGE_BUCKET);
+    const folderName = "Menzilla-storage/";
+    const fileUpload = bucket.file(folderName + file.originalname);
 
-  const stream = fileUpload.createWriteStream({
-    metadata: {
-      contentType: file.mimetype,
-    },
-  });
-
-  stream.on("error", (error) => {
-    console.log(error);
-  });
-  const data = await stream.on("finish", async () => {
-    const accessUrl = await fileUpload.getSignedUrl({
-      action: "read",
-      expires: "01-01-2025",
+    const stream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
     });
 
-    const hashedPassword = await bcrypt.hash(shopData?.password, 10);
-
-    let result = await ShopModal.create({
-      name: shopData?.shopName,
-      email: shopData?.email,
-      shopProfile: accessUrl[0],
-      phoneNumber: shopData?.phone,
-      address: shopData?.address,
-      zipCode: shopData?.zipCode,
-      password: hashedPassword,
+    stream.on("error", (error) => {
+      console.log(error);
     });
-    return result;
-  });
-  stream.end(file.buffer);
+    const data = await stream.on("finish", async () => {
+      const accessUrl = await fileUpload.getSignedUrl({
+        action: "read",
+        expires: "01-01-2025",
+      });
 
-  return { status: 201, message: "shop created successfully" };
+      const hashedPassword = await bcrypt.hash(shopData?.password, 10);
+
+      let result = await ShopModal.create({
+        name: shopData?.shopName,
+        email: shopData?.email,
+        shopProfile: accessUrl[0],
+        phoneNumber: shopData?.phone,
+        address: shopData?.address,
+        zipCode: shopData?.zipCode,
+        password: hashedPassword,
+      });
+      return result;
+    });
+    stream.end(file.buffer);
+
+    return { status: 201, message: "shop created successfully" };
+  } catch (error) {
+    return {
+      status: 500,
+      message: error.toString(),
+    };
+  }
 };
