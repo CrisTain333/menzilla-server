5c40b46176615ff30381fcf1ff799cad222fe80e
Sukanta Das <arnobh84@gmail.com>
2023-05-07 14:31:38 +0600

    some change again

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 const Sib = require("sib-api-v3-sdk");
 const client = Sib.ApiClient.instance;
-const apiKey = client.authentications["api-key"];
-apiKey.apiKey = process.env.API_KEY;
+const apiKey = client.authentications[process.env.API_KEY];
+// apiKey.apiKey = process.env.API_KEY;
 
 const createActivationToken = (user) => {
   return jwt.sign(user, process.env.JWT_SECRET, {
