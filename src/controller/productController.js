36116890a5b1ccfd7fac705fbd6506d8b9453bdb
Sukanta Das <arnobh84@gmail.com>
2023-05-18 20:31:07 +0600

    product controler and services added

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,3 +1,15 @@
-const createProduct = async () => {};
+const productServices = require("../services/productServices");
+const createProduct = async (req, res, next) => {
+  try {
+    const result = await productServices.createProductHandler(req, res);
+    res.send({
+      status: result?.status,
+      message: result?.message,
+    });
+  } catch (error) {
+    res.json({ status: 500, message: error?.message });
+    next(error);
+  }
+};
 
 module.exports = { createProduct };
