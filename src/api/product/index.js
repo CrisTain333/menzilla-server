ce83d2b93ed920ab92d9e3727460e19ef4bb8a43
Sukanta Das <arnobh84@gmail.com>
2023-05-18 20:36:44 +0600

    testing

--- src/api/product/index.js
+++ src/api/product/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { createProduct } = require("../../controller/productController");
+const upload = require("../../middleware/upload");
 const router = express.Router();
 
-router.post("/create-product", createProduct);
+router.post("/create-product", upload.array("images"), createProduct);
 
 module.exports = router;
