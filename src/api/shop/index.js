0ea78c712b42c33bf9ae03cb87d470a95465349a
Sukanta Das <arnobh84@gmail.com>
2023-05-28 20:09:08 +0600

    change in get seller function

--- src/api/shop/index.js
+++ src/api/shop/index.js
@@ -13,5 +13,6 @@ router.post("/register", newUploadSystem.single("shopProfile"), registerShop);
 router.post("/login", sellerLogin);
 router.post("/verify-email", verifySellerEmail);
 router.get("/seller", getSeller);
+// router.get('/:id')
 
 module.exports = router;
