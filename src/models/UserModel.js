fc510cf062e116e371a8fb9982d9c3bb45ec3513
Sukanta Das <arnobh84@gmail.com>
2023-05-30 20:02:48 +0600

    change again

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -35,28 +35,12 @@ const DataSchema = new mongoose.Schema(
       type: String,
       default: "user",
     },
-    addresses: [
-      {
-        country: {
-          type: String,
-        },
-        city: {
-          type: String,
-        },
-        address1: {
-          type: String,
-        },
-        address2: {
-          type: String,
-        },
-        zipCode: {
-          type: Number,
-        },
-        addressType: {
-          type: String,
-        },
-      },
-    ],
+    address1: {
+      type: String,
+    },
+    address2: {
+      type: String,
+    },
   },
   { versionKey: false }
 );
