186094b4749cc5c1c7fadb9304d0a7ad9d25cc1e
Sukanta Das <arnobh84@gmail.com>
2023-06-20 14:11:22 +0600

    create order service completed

--- src/services/orderServices.js
+++ src/services/orderServices.js
@@ -1,1 +1,43 @@
-exports.createOrder = async (req) => {};
+const OrderModal = require("../models/OrderModal");
+
+exports.createOrder = async (req) => {
+  const { cart, shippingAddress, user, totalPrice, paymentInfo } = req.body;
+
+  try {
+    //   group cart items by shopId
+    const shopItemsMap = new Map();
+
+    for (const item of cart) {
+      const shopId = item.shopId;
+      if (!shopItemsMap.has(shopId)) {
+        shopItemsMap.set(shopId, []);
+      }
+      shopItemsMap.get(shopId).push(item);
+    }
+
+    // create an order for each shop
+    const orders = [];
+
+    for (const [shopId, items] of shopItemsMap) {
+      const order = await OrderModal.create({
+        cart: items,
+        shippingAddress,
+        user,
+        totalPrice,
+        paymentInfo,
+      });
+      orders.push(order);
+    }
+
+    return {
+      message: "order created successfully",
+      status: 200,
+      orders,
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: "Fail to create order",
+    };
+  }
+};
