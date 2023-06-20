7a9bc5e8c22c8d07971ad86843e9cf2d7ee49625
Sukanta Das <arnobh84@gmail.com>
2023-06-20 12:00:52 +0600

    payment process controller and service added

--- src/api/payment/index.js
+++ src/api/payment/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-
+const paymentController = require("../../controller/paymentController");
 const router = express.Router();
+
+router.post("/payment-process", paymentController.paymentProcess);
 
 module.exports = router;
