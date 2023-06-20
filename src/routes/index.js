af0c74bd2cc858874b2156a371e034b37eb5360c
Sukanta Das <arnobh84@gmail.com>
2023-06-20 09:24:05 +0600

    working in payment and order

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,10 @@ const authRoute = require("../api/auth/index");
 const userRoute = require("../api/user/index");
 const shopRoute = require("../api/shop/index");
 const productRoute = require("../api/product/index");
-
+const paymentRoute = require("../api/payment/index");
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/shop", shopRoute);
 router.use("/product", productRoute);
-
+router.use("/payment", paymentRoute);
 module.exports = router;
