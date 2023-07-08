5a1e7ac11e6c7d569bc6fd1be26063895b590085
Sukanta Das <arnobh84@gmail.com>
2023-07-08 12:10:58 +0600

    availablebalance feild added for the currentSeller

--- src/services/shopServices.js
+++ src/services/shopServices.js
@@ -2,41 +2,76 @@ const { config } = require("../config/bucket.config");
 const ShopModal = require("../models/ShopModal");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { createActivationToken, sendEmail } = require("../utils/common/index");
-const { uploadMultipleFiles } = require("../middleware/uploadImage");
+const {
+  createActivationToken,
+  sendEmail,
+} = require("../utils/common/index");
+const {
+  uploadMultipleFiles,
+} = require("../middleware/uploadImage");
 
 exports.handleShopRegister = async (req, res) => {
   try {
     const shopData = req.body;
     const file = req.file;
 
-    const { shopName, email, password, address, phone, shopProfile, zipCode } =
-      shopData;
+    const {
+      shopName,
+      email,
+      password,
+      address,
+      phone,
+      shopProfile,
+      zipCode,
+    } = shopData;
 
     if (!shopName) {
-      return { status: 400, message: "shope name  is  required" };
+      return {
+        status: 400,
+        message: "shope name  is  required",
+      };
     } else if (!email) {
       return { status: 400, message: "email is  required" };
     } else if (!password) {
-      return { status: 400, message: "password is  required" };
+      return {
+        status: 400,
+        message: "password is  required",
+      };
     } else if (!address) {
-      return { status: 400, message: "address is  required" };
+      return {
+        status: 400,
+        message: "address is  required",
+      };
     } else if (!phone) {
       return { status: 400, message: "phone is  required" };
     } else if (!file) {
-      return { status: 400, message: "shop profile is  required" };
+      return {
+        status: 400,
+        message: "shop profile is  required",
+      };
     } else if (!zipCode) {
-      return { status: 400, message: "zipCode  is  required" };
+      return {
+        status: 400,
+        message: "zipCode  is  required",
+      };
     }
 
-    const existingUser = await ShopModal.findOne({ $or: [{ email }] });
+    const existingUser = await ShopModal.findOne({
+      $or: [{ email }],
+    });
     if (existingUser) {
-      return { status: 400, message: "Email already in use" };
+      return {
+        status: 400,
+        message: "Email already in use",
+      };
     }
     const shopImage = [file];
     const imageUrl = await uploadMultipleFiles(shopImage);
 
-    const hashedPassword = await bcrypt.hash(shopData?.password, 10);
+    const hashedPassword = await bcrypt.hash(
+      shopData?.password,
+      10
+    );
 
     const NewShopData = {
       name: shopData?.shopName,
@@ -49,7 +84,8 @@ exports.handleShopRegister = async (req, res) => {
       isEmailVerified: false,
     };
 
-    const activationToken = createActivationToken(NewShopData);
+    const activationToken =
+      createActivationToken(NewShopData);
     const activationUrl = `${process.env.FRONT_END_BASE_URL}/auth/seller-activation?token=${activationToken}`;
     try {
       await sendEmail(NewShopData, activationUrl);
@@ -59,7 +95,10 @@ exports.handleShopRegister = async (req, res) => {
       console.log("email send fail");
     }
     await ShopModal.create(NewShopData);
-    return { status: 201, message: "shop created successfully" };
+    return {
+      status: 201,
+      message: "shop created successfully",
+    };
   } catch (error) {
     console.log(error);
     return {
@@ -72,19 +111,30 @@ exports.handleShopRegister = async (req, res) => {
 // ---------------------- handle Verify Email ---------------------
 exports.handleSellerEmailVerify = async (req, res) => {
   const { token } = req.query;
-  const newShop = await jwt.verify(token, process.env.JWT_SECRET);
+  const newShop = await jwt.verify(
+    token,
+    process.env.JWT_SECRET
+  );
   if (!newShop) {
     return { message: "Invalid token", status: 400 };
   }
   const { email } = newShop;
 
   // Check if the user already exists
-  const existingShop = await ShopModal.findOne({ $or: [{ email }] });
+  const existingShop = await ShopModal.findOne({
+    $or: [{ email }],
+  });
 
   if (existingShop?.isEmailVerified) {
-    return { status: 400, message: "Email already Verified" };
+    return {
+      status: 400,
+      message: "Email already Verified",
+    };
   }
-  await ShopModal.findOneAndUpdate({ email }, { isEmailVerified: true });
+  await ShopModal.findOneAndUpdate(
+    { email },
+    { isEmailVerified: true }
+  );
 
   return { message: "user created", status: 201 };
 };
@@ -96,29 +146,42 @@ exports.handleSellerLogin = async (req, res) => {
   try {
     // all fields are required
     if ((!email, !password)) {
-      return { status: 401, message: "All fields are required" };
+      return {
+        status: 401,
+        message: "All fields are required",
+      };
     }
 
     const shop = await ShopModal.findOne({ email });
     // check the user exist in Database Or Not
     if (!shop) {
-      return { message: "Shop Details Not Found", status: 404 };
+      return {
+        message: "Shop Details Not Found",
+        status: 404,
+      };
     }
 
     if (!shop?.isEmailVerified) {
       return { message: "email not verified", status: 404 };
     }
 
-    const isMatch = await bcrypt.compare(password, shop?.password);
+    const isMatch = await bcrypt.compare(
+      password,
+      shop?.password
+    );
     //Check User Password
     if (!isMatch) {
       return { status: 401, message: "invalid credential" };
     }
 
     // Generate Token
-    const token = jwt.sign({ shopId: shop._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
+    const token = jwt.sign(
+      { shopId: shop._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
 
     // send token as response
     return {
@@ -149,6 +212,7 @@ exports.handleGetSeller = async (req, res) => {
         zipCode,
         isEmailVerified,
         createdAt,
+        availableBalance,
       } = seller;
 
       return {
@@ -165,6 +229,7 @@ exports.handleGetSeller = async (req, res) => {
           zipCode,
           isEmailVerified,
           createdAt,
+          availableBalance,
         },
       };
     } else {
