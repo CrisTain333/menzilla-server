4a2c77ec8382a772f583c714df5e7a946d6cbf71
Sukanta Das <arnobh84@gmail.com>
2023-07-10 14:27:01 +0600

    soket server created

--- src/app.js
+++ src/app.js
@@ -8,12 +8,17 @@ const connectToDatabase = require("./Database/ConnectToDatabase");
 const routes = require("./routes/index");
 
 const app = express();
+const server = require("http").createServer(app);
+const socketIO = require("socket.io");
+export const io = socketIO(server);
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const path = require("path");
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({ extended: true, limit: "50mb" })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 // Serve uploaded images
@@ -30,6 +35,10 @@ app.get("/", async (req, res) => {
 // Entrance
 app.use("/api/v1", routes);
 
+
+
 app.listen(PORT, () => {
-  console.log(`⚡ Server Fire On http://localhost:${PORT}`.cyan);
+  console.log(
+    `⚡ Server Fire On http://localhost:${PORT}`.cyan
+  );
 });
